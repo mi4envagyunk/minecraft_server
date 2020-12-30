b7b8ac22fc801bce8e938e9ac9f5156bb0998d91
mi4envagyunk <dame da ne dame yo dame na no yo anta ga suki de suki sugite>
2020-12-30 21:26:27 +0100

    Added update script

---
+++ UpdateServer_FromCloud.bat
@@ -0,0 +1,8 @@
+@echo off
+git init
+git config --global user.email "mi4envagyunk@gmail.com"
+git config --global user.name "mi4envagyunk"
+git config --global user.email "dame da ne dame yo dame na no yo anta ga suki de suki sugite"
+git remote add origin https://mi4envagyunk:"dame da ne dame yo dame na no yo anta ga suki de suki sugite"@github.com/mi4envagyunk/minecraft_server.git
+git pull origin master
+pause
